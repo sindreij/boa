3e77b84072da8b6381a5d94aa67e6946ba133906
jedel1043 <jedel0124@gmail.com>
2021-09-17 23:08:51 -0500

    Implement `CompletionRecord` type

--- boa/src/exec/mod.rs
+++ boa/src/exec/mod.rs
@@ -10,6 +10,61 @@ pub trait Executable {
     fn run(&self, context: &mut Context) -> JsResult<JsValue>;
 }
 
+/// Enum representing all the possible completion results that can be returned
+/// when interpreting Javascript code.
+pub(crate) enum CompletionKind {
+    Normal,
+    Return,
+    Break(Option<Box<str>>),
+    Continue(Option<Box<str>>),
+    Throw,
+}
+
+/// The `Completion Record` Specification Type
+///
+/// The `Completion` type is a `Record` used to explain the runtime propagation of
+/// values and control flow such as the behaviour of statements
+/// (break, continue, return and throw) that perform nonlocal transfers of control.
+///
+/// More information:
+///  - [ECMAScript reference][spec]
+///
+/// [spec]: https://tc39.es/ecma262/#sec-completion-record-specification-type
+pub(crate) struct CompletionRecord {
+    pub(crate) kind: CompletionKind,
+    pub(crate) value: Option<JsValue>,
+}
+
+impl CompletionRecord {
+    /// Abstract operation `UpdateEmpty ( completionRecord, value )`
+    ///
+    /// More information:
+    ///  - [ECMAScript reference][spec]
+    ///
+    /// [spec]: https://tc39.es/ecma262/#sec-updateempty
+    pub(crate) fn update_empty(mut self, value: JsValue) -> Self {
+        if self.value.is_none() {
+            self.value = Some(value)
+        }
+        self
+    }
+}
+
+impl From<JsResult<JsValue>> for CompletionRecord {
+    fn from(result: JsResult<JsValue>) -> Self {
+        match result {
+            Ok(val) => Self {
+                value: Some(val),
+                kind: CompletionKind::Normal,
+            },
+            Err(err) => Self {
+                value: Some(err),
+                kind: CompletionKind::Throw,
+            },
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub(crate) enum InterpreterState {
     Executing,
